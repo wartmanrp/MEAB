196b76ead62531beb3f2e8820ebf40d7e906d10d
Powers Wartman <powers.wartman@gmail.com>
2017-01-27 15:01:25 -0500

    Stufff?

--- MEAB/App_Start/BundleConfig.cs
+++ MEAB/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace MEAB
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.sandstone.min.css",
                       "~/Content/site.css"));
         }
     }
